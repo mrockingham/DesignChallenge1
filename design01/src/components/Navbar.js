32ef95de6da7290c9ebbec37ee782e4febc4afb2
Michael Rockingham <mrockingham2@gmail.com>
2021-01-26 13:50:17 -0500

    add dep

--- design01/src/components/Navbar.js
+++ design01/src/components/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined';
 import PersonIcon from '@material-ui/icons/Person';
 import SearchIcon from '@material-ui/icons/Search';
-import FormatAlignLeftIcon from '@material-ui/icons/FormatAlignLeft';
+
 import Divider from '@material-ui/core/Divider';
-import Grid from '@material-ui/core/Grid';
+
 import './NavBar.css'
 
 export const Navbar = () => {
